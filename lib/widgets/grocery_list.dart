38cd6534afc2ea267b495ed1873b7322be552671
Subhajit Sarkar <sarkarsubhajit9499@gmail.com>
2023-08-23 22:51:35 +0530

    adding a loading state

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -15,6 +15,7 @@ class GroceryList extends StatefulWidget {
 
 class _GroceryListState extends State<GroceryList> {
   List<GroceryItem> _groceryItems = [];
+  var _isLoading = true;
 
   @override
   void initState() {
@@ -42,6 +43,7 @@ class _GroceryListState extends State<GroceryList> {
     }
     setState(() {
       _groceryItems = loadedItems;
+      _isLoading = false;
     });
   }
 
@@ -71,6 +73,12 @@ class _GroceryListState extends State<GroceryList> {
     Widget content = const Center(
       child: Text('No  Items added yet'),
     );
+
+    if (_isLoading) {
+      content = const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
 
     if (_groceryItems.isNotEmpty) {
       content = ListView.builder(
