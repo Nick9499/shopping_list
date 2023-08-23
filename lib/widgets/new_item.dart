70934741ac870faa9bd162e3e108b0b6d6d87d6f
Subhajit Sarkar <sarkarsubhajit9499@gmail.com>
2023-08-23 16:47:18 +0530

    getting form access via global key

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/categories.dart';
 
@@ -10,6 +9,12 @@ class NewItem extends StatefulWidget {
 }
 
 class _NewItemState extends State<NewItem> {
+  final _formKey = GlobalKey<FormState>();
+
+  void _saveItem() {
+    _formKey.currentState!.validate();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,92 +24,96 @@ class _NewItemState extends State<NewItem> {
       body: Padding(
         padding: const EdgeInsets.all(12),
         child: Form(
+            key: _formKey,
             child: Column(
-          children: [
-            TextFormField(
-              maxLength: 50,
-              decoration: const InputDecoration(
-                label: Text('Name'),
-              ),
-              validator: (value) {
-                if (value == null ||
-                    value.isEmpty ||
-                    value.trim().length <= 1 ||
-                    value.trim().length > 50) {
-                  return 'Must be between 1 & 50 characters';
-                }
-                return null;
-              },
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
               children: [
-                Expanded(
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                      label: Text('Quantity'),
-                    ),
-                    initialValue: '1',
-                    validator: (value) {
-                      if (value == null ||
-                          value.isEmpty ||
-                          int.tryParse(value) == null ||
-                          int.tryParse(value)! <= 0) {
-                        return 'Must be a valid positive number';
-                      }
-                      return null;
-                    },
+                TextFormField(
+                  maxLength: 50,
+                  decoration: const InputDecoration(
+                    label: Text('Name'),
                   ),
+                  validator: (value) {
+                    if (value == null ||
+                        value.isEmpty ||
+                        value.trim().length <= 1 ||
+                        value.trim().length > 50) {
+                      return 'Must be between 1 & 50 characters';
+                    }
+                    return null;
+                  },
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          label: Text('Quantity'),
+                        ),
+                        initialValue: '1',
+                        keyboardType: TextInputType.number,
+                        validator: (value) {
+                          if (value == null ||
+                              value.isEmpty ||
+                              int.tryParse(value) == null ||
+                              int.tryParse(value)! <= 0) {
+                            return 'Must be a valid positive number';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Expanded(
+                      child: DropdownButtonFormField(
+                        items: [
+                          for (final category in categories.entries)
+                            DropdownMenuItem(
+                              value: category.value,
+                              child: Row(
+                                children: [
+                                  Container(
+                                    width: 16,
+                                    height: 16,
+                                    color: category.value.color,
+                                  ),
+                                  const SizedBox(
+                                    width: 6,
+                                  ),
+                                  Text(
+                                    category.value.title,
+                                  ),
+                                ],
+                              ),
+                            ),
+                        ],
+                        onChanged: (value) {},
+                      ),
+                    ),
+                  ],
                 ),
                 const SizedBox(
-                  width: 8,
+                  height: 12,
                 ),
-                Expanded(
-                  child: DropdownButtonFormField(
-                    items: [
-                      for (final category in categories.entries)
-                        DropdownMenuItem(
-                          value: category.value,
-                          child: Row(
-                            children: [
-                              Container(
-                                width: 16,
-                                height: 16,
-                                color: category.value.color,
-                              ),
-                              const SizedBox(
-                                width: 6,
-                              ),
-                              Text(
-                                category.value.title,
-                              ),
-                            ],
-                          ),
-                        ),
-                    ],
-                    onChanged: (value) {},
-                  ),
-                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        _formKey.currentState!.reset();
+                      },
+                      child: const Text('Reset'),
+                    ),
+                    ElevatedButton(
+                      onPressed: _saveItem,
+                      child: const Text('Add Item'),
+                    ),
+                  ],
+                )
               ],
-            ),
-            const SizedBox(
-              height: 12,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                TextButton(
-                  onPressed: () {},
-                  child: const Text('Reset'),
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const Text('Add Item'),
-                ),
-              ],
-            )
-          ],
-        )),
+            )),
       ),
     );
   }
