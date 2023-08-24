370f32da23d49c84a25573e75d016d43f05d765e
Subhajit Sarkar <sarkarsubhajit9499@gmail.com>
2023-08-24 19:15:31 +0530

    handling the no data case

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -35,6 +35,13 @@ class _GroceryListState extends State<GroceryList> {
         _error = 'Failed to fetch data, please try again';
       });
     }
+
+    if (response.body == 'null') {
+      setState(() {
+        _isLoading = false;
+      });
+      return;
+    }
     final Map<String, dynamic> listData = json.decode(response.body);
     final List<GroceryItem> loadedItems = [];
     for (final item in listData.entries) {
